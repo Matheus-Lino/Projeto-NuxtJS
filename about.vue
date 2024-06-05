ea93da4122c78e21054f6fc76e1bcb183c22276d
Matheus Lino <126609671+Matheus-Lino@users.noreply.github.com>
2024-06-05 09:59:14 -0300

    Add files via upload

---
+++ about.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque, expedita a recusandae voluptates corrupti hic at distinctio beatae accusamus dignissimos aspernatur animi autem laborum voluptatem harum perferendis ipsa optio nihil?</p>
+        <div>{{ data }}</div>
+    </div>
+</template>
+
+<script setup>
+    const { data } = await useFetch('./api/currency/BRL')
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0px;
+    }
+</style>
